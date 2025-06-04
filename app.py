45f04792dc584880f81a47829ffc8b84a1eb2526
Martin Gonzales <jbmgonzales1312@gmail.com>
2025-06-04 03:16:22 -0500

    Configurado puerto dinámico para despliegue en Render

--- app.py
+++ app.py
@@ -50,4 +50,6 @@ def api_ubicaciones():
     return jsonify(usuarios)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
