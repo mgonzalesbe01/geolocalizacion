3a937545faa2e766547db64f97bfb193f81b0b52
Martin Gonzales <jbmgonzales1312@gmail.com>
2025-06-05 03:15:55 -0500

    Mejoras version nueve

--- app.py
+++ app.py
@@ -184,6 +184,4 @@ def favicon():
 
 # Iniciar servidor localmente (solo desarrollo)
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(host='0.0.0.0', port=5000)
