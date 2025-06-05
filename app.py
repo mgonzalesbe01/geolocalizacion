02ca3fd8313ae919af8a34370048fdf93f0cc646
Martin Gonzales <jbmgonzales1312@gmail.com>
2025-06-05 03:32:47 -0500

    Mejoras version diez

--- app.py
+++ app.py
@@ -184,4 +184,5 @@ def favicon():
 
 # Iniciar servidor localmente (solo desarrollo)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
