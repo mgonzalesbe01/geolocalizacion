cf199bd5f26bb3e451959aa60feac46e9c07c386
Martin Gonzales <jbmgonzales1312@gmail.com>
2025-06-06 14:41:15 -0500

    Version desde cero 2

--- app.py
+++ app.py
@@ -77,3 +77,7 @@ def api_ubicaciones():
                 'precision': d.precision
             }
     return jsonify(data)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
